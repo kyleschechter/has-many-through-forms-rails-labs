d3fc1284692a46728e66be95b09cc9290234b037
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-09-27 16:16:56 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
-
+  def categories_attributes=(attributes)
+    attributes.values.each do |attribute|
+      if attribute[:name].length > 0
+        self.categories.build(attribute)
+      end
+    end
+  end
 end
